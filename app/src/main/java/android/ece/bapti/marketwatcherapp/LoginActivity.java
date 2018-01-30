2fc8f12f283192ad18830f3478610cd480e4abd5
Pingkachu <matthieu.hannequin@gmail.com>
2018-01-30 12:22:55 +0100

    Connexion

--- app/src/main/java/android/ece/bapti/marketwatcherapp/LoginActivity.java
+++ app/src/main/java/android/ece/bapti/marketwatcherapp/LoginActivity.java
@@ -30,13 +30,52 @@ public class LoginActivity extends AppCompatActivity {
         mPasswordView = (EditText) findViewById(R.id.logPassword);
         mSubmitView = (Button) findViewById(R.id.logSubmit);
 
-        mSubmitView.setOnClickListener(new View.OnClickListener(){
+        // An appropriate listener is assigned to the views that need it
+        mSubmitView.setOnClickListener(submitListener);
+
+    }
+
+    public OnClickListener submitListener = new OnclickListener () {
             @Override
             public void onClick(View view){
 
                 // To unable lately
                 /* HomeIntent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(HomeIntent); */
+
+                //To unable Lately
+                /*
+                mail = mEmailView.getText().toString();
+                pw = mPasswordView.getText().toString();
+                Log.i("mail : " +mail+ " ; pw : " +pw);
+                try {
+                  // Encoding Query parameters
+                  String donnees = URLEncoder.encode("login", "ISO-8859-1")+ "="+URLEncoder.encode(mail, "ISO-8859-1");
+                  donnees += "&"+URLEncoder.encode("password", "ISO-8859-1")+ "=" + URLEncoder.encode(pw, "ISO-8859-1");
+                  
+                  // We sent data on a distant adress
+                  String api = Connexion.GET("http://..."+donnees);
+                  Log.i("api", "result request : "+api);
+                  info = new JSONObject(api);
+                  connect = info.getBoolean("connect");
+                } catch (Exception e) {
+                  e.printStackTrace();
+                }
+                //If the account exists
+                if( connect == true)
+                {
+                    //Start new activity
+                    HomeIntent = new Intent(self, MainActivity.class);
+                    startActivity(HomeIntent);
+
+                }
+                else if (connect ==false)
+                {
+                    Toast.makeText(getApplicationContext(),"Identifiants incorrects",Toast.LENGTH_SHORT).show();
+                    // mEmailView.setText(null);
+                    mPasswordView.setText(null);
+                }
+                */
 
                 // To Activate lately
                 if(mEmailView.getText().toString().equals(email) &&
@@ -53,6 +92,44 @@ public class LoginActivity extends AppCompatActivity {
                 }
 
             }
-        });
+        };
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+              // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
